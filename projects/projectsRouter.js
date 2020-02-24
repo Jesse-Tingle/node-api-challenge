f97e2e0ef0ef37cf294761bffcde2e8440fe509d
Jesse-Tingle <jtingle0@gmail.com>
2020-02-23 19:55:34 -0500

    GET by ID route functioning for projects

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -16,7 +16,17 @@ router.get("/", async (req, res) => {
 	}
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", async (req, res) => {
+	const { id } = req.params;
+	const projectList = await projects.get(id);
+	if (projectList) {
+		return res.status(200).json(projectList);
+	} else {
+		return res
+			.status(500)
+			.json({ error: "The list of projects could not be retrieved." });
+	}
+});
 
 router.post("/", async (req, res) => {
 	try {
@@ -24,7 +34,9 @@ router.post("/", async (req, res) => {
 		console.log("name: ", name);
 		console.log("description: ", description);
 		console.log("completed: ", completed);
-		res.status(201).json(await userDb.insert({ name, description, completed }));
+		res
+			.status(201)
+			.json(await projects.insert({ name, description, completed }));
 	} catch (error) {
 		res.status(500).json({
 			error,
@@ -38,5 +50,9 @@ router.put("/:id", (req, res) => {});
 
 router.delete("/:id", (req, res) => {});
 
-function validateId() {}
+// function validateId() {
+//     return (req, res, next) => {
+//         projects.
+//     }
+// }
 module.exports = router;
